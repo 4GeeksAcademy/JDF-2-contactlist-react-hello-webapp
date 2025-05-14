515795c868d8781a2332496ab1d66e2197d95be6
Juandomz91 <juandomz87@gmail.com>
2025-05-14 18:53:05 +0000

    contacts y store  hechos

---
+++ src/components/contacts.js
@@ -0,0 +1,44 @@
+export async function createAgenda () {
+    const response = await fetch(`https://playground.4geeks.com/contact/agendas/JoanDo`,{
+    method: "POST",
+    headers: {"Content-Type": "application/json" },
+    });
+ 
+
+    if(!response.ok) throw new Error("Error al crear la agenda")
+    
+}
+
+
+export async function getContacts() {
+    const response = await fetch(`https://playground.4geeks.com/contact/agendas/JoanDo/contacts`);
+    if (!response.ok) throw new Error("Error al obtener contactos");
+    return await response.json();
+}
+
+export async function createContact(contact) {
+    const response = await fetch(`https://playground.4geeks.com/contact/agendas/JoanDo/contacts`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...contact, agenda_slug: JoanDo })
+    });
+    if (!response.ok) throw new Error("Error al crear contacto");
+    return await response.json();
+}
+
+export async function updateContact(contact) {
+    const response = await fetch(`https://playground.4geeks.com/contact/agendas/JoanDo/contacts/${contact.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(contact)
+    });
+    if (!response.ok) throw new Error("Error al actualizar contacto");
+    return await response.json();
+}
+
+export async function deleteContact(id) {
+    const response = await fetch(`https://playground.4geeks.com/contact/agendas/JoanDo/contacts/${id}`, {
+        method: "DELETE"
+    });
+    if (!response.ok) throw new Error("Error al eliminar contacto");
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
+  
 
 	return (
 		<div className="text-center mt-5">
